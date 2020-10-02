72e02a7e92b3428e53d55992220eb9519b855ff6
cooperprivett <69913040+cooperprivett@users.noreply.github.com>
2020-10-02 11:14:11 -0400

    Friday Class

--- drawing.py
+++ drawing.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 from tkinter import Tk, Canvas, mainloop
 
 root = Tk()
@@ -22,3 +23,29 @@ def react_to_click(event):
 
 c.bind('<Button-1>', react_to_click)
 mainloop()
+=======
+from tkinter import Tk, Canvas, mainloop
+
+root = Tk()
+c = Canvas(root, width=500, height=500)
+c.pack()
+
+# Put drawing here!
+c.create_rectangle(50, 50, 500, 500, fill='red')
+c.create_oval(350, 50, 500, 200, fill='green')
+c.create_rectangle(50, 450, 500, 500, fill='blue')
+
+# for y in range(50, 500, 50):
+#     c.create_rectangle(0, y, 500, y)
+#     c.create_rectangle(18, y + 10, text=str(y))
+
+# for x in range(50, 500, 50):
+#     c.create_line(x, 0, x, 500)
+#     c.create_text(x + 18, 10, text=str(x))
+
+def react_to_click(event):
+    root.quit()
+
+c.bind('<Button-1>', react_to_click)
+mainloop()
+>>>>>>> 51ec87a... Friday Class
