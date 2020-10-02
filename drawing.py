68a0be960264eec5d19d47184950014331816b19
cooperprivett <69913040+cooperprivett@users.noreply.github.com>
2020-10-02 11:15:55 -0400

    FRI Class

--- drawing.py
+++ drawing.py
@@ -35,13 +35,13 @@ c.create_rectangle(50, 50, 500, 500, fill='red')
 c.create_oval(350, 50, 500, 200, fill='green')
 c.create_rectangle(50, 450, 500, 500, fill='blue')
 
-# for y in range(50, 500, 50):
-#     c.create_rectangle(0, y, 500, y)
-#     c.create_rectangle(18, y + 10, text=str(y))
+for y in range(50, 500, 50):
+    c.create_line(0, y, 500, y)
+    c.create_text(18, y + 10, text=str(y))
 
-# for x in range(50, 500, 50):
-#     c.create_line(x, 0, x, 500)
-#     c.create_text(x + 18, 10, text=str(x))
+for x in range(50, 500, 50):
+    c.create_line(x, 0, x, 500)
+    c.create_text(x + 18, 10, text=str(x))
 
 def react_to_click(event):
     root.quit()
