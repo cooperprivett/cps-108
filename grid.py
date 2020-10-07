20fd16f3a91a41b88ba403728fefe36b5be877ed
cooperprivett <69913040+cooperprivett@users.noreply.github.com>
2020-10-07 11:44:48 -0400

    Add files via upload

---
+++ grid.py
@@ -0,0 +1,24 @@
+from tkinter import Tk, Canvas, mainloop
+
+root = Tk()
+c = Canvas(root, width=500, height=500)
+c.pack()
+
+# Put drawing here!
+c.create_rectangle(50, 50, 250, 250, fill='red')
+c.create_oval(350, 50, 250, 250, 350, 500, fill='green')
+c.create_rectangle(50, 50, 300, 500, fill='blue')
+
+for y in range(50, 250, 50):
+    c.create_line(0, y, 500, y)
+    c.create_text(18, y + 10, text=str(y))
+
+for x in range(50, 250, 50):
+    c.create_line(x, 0, x, 500)
+    c.create_text(x + 18, 10, text=str(x))
+
+def react_to_click(event):
+    root.quit()
+
+c.bind('<Button-1>', react_to_click)
+mainloop()
